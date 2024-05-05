8a062e23a646d50b98a08de47acbb615a0bdfd0d
noheartpen <3104408599@qq.com>
2024-05-05 10:19:38 +0900

    feat: 加载数据库

--- main.ts
+++ main.ts
@@ -50,20 +50,84 @@ function clearTimerWhenControlled(event: KeyboardEvent) {
 
 interface MyPluginSettings {
 	mySetting: string;
+	conjugateRuleDbFileName: string
+	specialRuleDbFileName: string
+	indexDbFileName: string;
 }
 
 const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+	indexDbFileName: '.obsidian/plugins/obsidian-sample-plugin/rules/index.json',
+	conjugateRuleDbFileName: '.obsidian/plugins/obsidian-sample-plugin/rules/conjugate_rule.json',
+	specialRuleDbFileName: '.obsidian/plugins/obsidian-sample-plugin/rules/special_rule.json',
+	mySetting: 'default',
+}
+
+function validateDB(db: any): boolean {
+	return typeof db === "object" && !Array.isArray(db);
 }
 
 export default class MyPlugin extends Plugin {
 	settings: MyPluginSettings;
+	indexDb: { [key: string]: string[] };
+	conjugateRuleDb: { [key: string]: string[] };
+	specialRuleDb: { [key: string]: string[] };
+
+	async loadDB(dbFileName: string): Promise<{ [key: string]: string[] }> {
+		try {
+			if (await this.app.vault.adapter.exists(dbFileName)) {
+				const data = await this.app.vault.adapter.read(dbFileName);
+				const db = JSON.parse(data);
+
+				// 数据结构验证
+				if (validateDB(db)) {
+					console.log("Database loaded successfully:", db);
+					return db;
+				} else {
+					console.error("Invalid database structure detected.");
+					// 返回默认值或者抛出错误，根据你的需求来决定
+					return {};
+				}
+			} else {
+				console.warn("Database file not found. Creating a new one.");
+				return {}; // 返回空对象作为默认值
+			}
+		} catch (error) {
+			console.error("Error loading database:", error);
+			throw error; // 抛出错误，以便上层代码处理
+		}
+	}
 
 	async onload() {
 		this.registerDomEvent(window, 'keyup', (event) => openSearchWhenDoubleShift(event, this.app))
 		this.registerDomEvent(window, 'keydown', (event) => clearTimerWhenControlled(event))
 		await this.loadSettings();
+		try {
+			this.indexDb = await this.loadDB(this.settings.indexDbFileName);
+		} catch (e) {
+			console.error(
+				"Remember Cursor Position plugin can\'t read database: " + e
+			);
+			this.indexDb = {};
+		}
 
+		try {
+			this.conjugateRuleDb = await this.loadDB(this.settings.conjugateRuleDbFileName);
+
+		} catch (e) {
+			console.error(
+				"Remember Cursor Position plugin can\'t read database: " + e
+			);
+			this.conjugateRuleDb = {};
+		}
+
+		try {
+			this.specialRuleDb = await this.loadDB(this.settings.specialRuleDbFileName);
+		} catch (e) {
+			console.error(
+				"Remember Cursor Position plugin can\'t read database: " + e
+			);
+			this.conjugateRuleDb = {};
+		}
 		// This creates an icon in the left ribbon.
 		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
 			// Called when the user clicks the icon.
