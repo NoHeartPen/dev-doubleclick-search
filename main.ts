ec2ed80e14b8bad5f80e630e702936dcc28db423
noheartpen <3104408599@qq.com>
2025-04-20 23:05:06 +0900

    update: init

---
+++ main.ts
@@ -0,0 +1,409 @@
+import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { TFile } from 'obsidian';
+
+let lastKeyupTime = 0;
+let lastKeyWasDouble: boolean
+
+/**
+ * 允许用户自定义按键
+ * @param key 触发分析的按键
+ */
+function setUserDefinedKey(key: string) {
+	// TODO 检查用户的自定义按键是否合法
+	PLUGIN_SETTINGS.doubleClickedKey = key;
+	console.log(`用户设置的双击按键为：${PLUGIN_SETTINGS.doubleClickedKey}`);
+}
+
+/**
+ * 注册双击监听事件
+ * @param event 
+ * @param app 
+ */
+function openSearchWhenDoubleClicked(event: KeyboardEvent, app: App) {
+	const key = event.key;
+	if (key !== PLUGIN_SETTINGS.doubleClickedKey) {
+		lastKeyupTime = 0;
+		return;
+	}
+
+	if (lastKeyWasDouble) {
+		lastKeyWasDouble = false;
+		return;
+	}
+
+	if (Date.now() - lastKeyupTime < 500) {
+		new Notice(`分析中...`);
+		lastKeyupTime = 0;
+		getCursorWord().then((cursorWord) => {
+			if (cursorWord === undefined) {
+				console.log("cursor word is undefined");
+				return;
+			}
+			console.log("cursor word is: ", cursorWord);
+			doSearch(cursorWord);
+		}).catch((error) => {
+			console.error("get cursor word with error:", error);
+		});
+		return;
+	}
+	lastKeyupTime = Date.now();
+}
+
+/** 
+ * 用户双击时自动处理逻辑
+ */
+function clearTimerWhenDoubleClicked(event: KeyboardEvent) {
+	if (event.key !== PLUGIN_SETTINGS.doubleClickedKey) {
+		lastKeyWasDouble = true;
+	}
+}
+
+function getCursorContextAndIndex(): { context: string, cursorIndex: number } | undefined {
+	const view = this.app.workspace.getActiveViewOfType(MarkdownView);
+	if (view) {
+		const editor = view.editor;
+		const selection = editor.getSelection();
+		/**
+		 * 光标所在行的所有文本，用于分析光标附近文本对应的辞书形
+		 */
+		let context = "";
+		/**
+		 * 光标所在的位置，用于计算形态素分结果
+		 */
+		let cursorIndex = 0;
+		if (selection === "") {
+			const cursor = editor.getCursor();
+			// 获取光标所在行的所有文本
+			const cursorLineText = editor.getRange({ line: cursor.line, ch: 0 }, { line: cursor.line, ch: editor.getLine(cursor.line).length });
+			context = cursorLineText
+			cursorIndex = cursor.ch;
+		} else {
+			// 如果用户有选中文本，那么直接提交选中的文本
+			// TODO 笑了这种情况下 cursorIndex 直接使用 0 就行了么
+			context = selection;
+			cursorIndex = 0;
+		}
+		// TODO 删除文本中的 markdown 语法，比如 **bold**，*italic*，`code`,>,
+		return { context, cursorIndex };
+	} else {
+		console.error("No active MarkdownView found.");
+		return;
+	}
+}
+/**
+ * 获取光标附近的单词
+ */
+async function getCursorWord(): Promise<string | undefined> {
+	const context = getCursorContextAndIndex()?.context ?? "";
+	const cursorIndex = getCursorContextAndIndex()?.cursorIndex ?? 0;
+	console.log(`context: ${context}, cursorIndex: ${cursorIndex}`);
+	const cursorWord = await analyzeCursorWord(context, cursorIndex)
+	console.log(`cursorWord: ${cursorWord}`);
+	// 
+	// TODO context 和 cursorWord 写入到 MonoKakido History.md 中
+	if (cursorWord !== undefined) {
+		writeToFile('MonoKakido History.md', context, cursorWord);
+	}
+	return cursorWord;
+}
+async function writeToFile(fileName: string, context: string, cursorWord: string): Promise<void> {
+	const activeFile = this.app.workspace.getActiveFile();
+	const vault = this.app.vault;
+	const file = vault.getAbstractFileByPath(fileName);
+	const backLink = `[[${activeFile.basename}]]`;
+	// 允许用户通过设置自定义
+	const noteContent = `> ${context} ${backLink} \n> ${cursorWord} \n> メモ：\n\n`; // 添加双向链接
+	if (activeFile) {
+		if (file instanceof TFile) {
+			await vault.append(file, noteContent);
+		} else {
+			await vault.create(fileName, noteContent);
+		}
+		console.log(`Content written to file: ${fileName}`);
+	} else {
+		console.log("No active file found. Cannot create back link.");
+	}
+}
+
+/**
+ * 获取光标处附近的英文单词
+ * @param context 文本域的内容
+ * @returns 光标附近的英文单词，如果没有则返回空字符串
+ */
+export function getCursorEnglishWord(context: string, cursorIndex: number): string {
+	let start: number = cursorIndex;
+	let end: number = cursorIndex;
+
+	// 向前扫描，找到单词起点
+	while (start > 0 && /\S/.test(context[start - 1])) {
+		start--;
+	}
+	// 向后扫描，找到单词终点
+	while (end < context.length && /\S/.test(context[end])) {
+		end++;
+	}
+
+	// 提取并返回光标附近的单词
+	return context.substring(start, end).trim();
+}
+
+
+/**
+ * 形态素分析
+ * @param context 
+ * @param cursorIndex 
+ * @returns 
+ */
+async function analyzeCursorWord(context: string, cursorIndex: number): Promise<string | undefined> {
+	// TODO 如果不包含任何假名，那么直接通过空格推导
+	if (!context.match(/[\u3040-\u309F\u30A0-\u30FF]/)) {
+		return getCursorEnglishWord(context, cursorIndex);
+	}
+	const response = await fetch(PLUGIN_SETTINGS.morphemeAnalysisAPI, {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json'
+		},
+		body: JSON.stringify({
+			sentence: context,
+			cursor_index: cursorIndex
+		})
+	});
+
+	if (!response.ok) {
+		console.error('HTTP error:', response.status);
+		return;
+	}
+
+	const data = await response.json();
+	// 返回的数据格式： {jishokei: 'かける'}
+	console.log(data);
+	const cursorWords = data.jishokei;
+	return cursorWords;
+}
+
+
+/**
+ * 执行搜索动作
+ * @param word 推导的辞书形
+ */
+async function doSearch(word: string) {
+	if (PLUGIN_SETTINGS.searchByOpenUrl === true) {
+		openDictUrl(word)
+	} else {
+		await write2ClipBoard(word);
+	}
+}
+
+
+interface PluginSettingsInterface {
+	/**
+	 * 调用辞書的 URL
+	 */
+	dictURL: string;
+	/**
+	 * 是否调用 URL Scheme 查询
+	 */
+	searchByOpenUrl: boolean;
+	/**
+	 * 形态素分析 API
+	 */
+	morphemeAnalysisAPI: string;
+	/**
+	 * 双击指定的按键触发搜索
+	 */
+	doubleClickedKey: string;
+}
+
+const PLUGIN_SETTINGS: PluginSettingsInterface = {
+	// 默认使用 物書堂
+	dictURL: 'mkdictionaries:///?text=<text_to_search>',
+	// 默认不使用剪贴板查询模式
+	searchByOpenUrl: false,
+	// 如果你使用
+	// http://127.0.0.1:8000/
+	morphemeAnalysisAPI: 'https://fast-mikann-api.vercel.app/',
+	// 默认按键设定为 Option 键（在 Windows 上是 Alt 键）
+	doubleClickedKey: 'Alt'
+}
+
+export default class MonoKakidoCopilotPlugin extends Plugin {
+	settings: PluginSettingsInterface;
+
+
+	async onload() {
+		this.registerDomEvent(window, 'keyup', (event) => openSearchWhenDoubleClicked(event, this.app))
+		this.registerDomEvent(window, 'keydown', (event) => clearTimerWhenDoubleClicked(event))
+
+		// Open History
+		const ribbonIconEl = this.addRibbonIcon('magnifying-glass', 'Monokakido Copilot', (evt: MouseEvent) => {
+			const vault = this.app.vault;
+
+			// TODO 只支持指定文件
+			const filePath = 'MonoKakido History.md';
+
+			// 查找并打开文件
+			const file = vault.getAbstractFileByPath(filePath);
+			if (file && file instanceof TFile) {
+				this.app.workspace.openLinkText(filePath, '', true);
+			} else {
+				vault.create(filePath, '# MonoKakido History\n\nThis document is used to history.');
+				new Notice('File created: ' + filePath);
+				this.app.workspace.openLinkText(filePath, '', true);
+			}
+		});
+		ribbonIconEl.addClass('my-plugin-ribbon-class');
+
+
+		this.addCommand({
+			id: 'open-sample-modal-simple',
+			name: 'Open sample modal (simple)',
+			callback: () => {
+				new SampleModal(this.app).open();
+			}
+		});
+		// This adds an editor command that can perform some operation on the current editor instance
+		this.addCommand({
+			id: 'sample-editor-command',
+			name: 'Sample editor command',
+			editorCallback: (editor: Editor, view: MarkdownView) => {
+				console.log(editor.getSelection());
+				editor.replaceSelection('Sample Editor Command');
+			}
+		});
+		// This adds a complex command that can check whether the current state of the app allows execution of the command
+		this.addCommand({
+			id: 'open-sample-modal-complex',
+			name: 'Open sample modal (complex)',
+			checkCallback: (checking: boolean) => {
+				// Conditions to check
+				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
+				if (markdownView) {
+					// If checking is true, we're simply "checking" if the command can be run.
+					// If checking is false, then we want to actually perform the operation.
+					if (!checking) {
+						new SampleModal(this.app).open();
+					}
+
+					// This command will only show up in Command Palette when the check function returns true
+					return true;
+				}
+			}
+		});
+
+		// This adds a settings tab so the user can configure various aspects of the plugin
+		this.addSettingTab(new SettingTab(this.app, this));
+
+		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
+		// Using this function will automatically remove the event listener when this plugin is disabled.
+		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
+			console.log('click', evt);
+		});
+
+		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
+		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+	}
+
+	onunload() {
+
+	}
+
+	async loadSettings() {
+		this.settings = Object.assign({}, PLUGIN_SETTINGS, await this.loadData());
+	}
+
+	async saveSettings() {
+		await this.saveData(this.settings);
+	}
+}
+
+class SampleModal extends Modal {
+	constructor(app: App) {
+		super(app);
+	}
+
+	onOpen() {
+		const { contentEl } = this;
+		contentEl.setText('Woah!');
+	}
+
+	onClose() {
+		const { contentEl } = this;
+		contentEl.empty();
+	}
+}
+
+class SettingTab extends PluginSettingTab {
+	plugin: MonoKakidoCopilotPlugin;
+
+	constructor(app: App, plugin: MonoKakidoCopilotPlugin) {
+		super(app, plugin);
+		this.plugin = plugin;
+	}
+
+	display(): void {
+		const { containerEl } = this;
+
+		containerEl.empty();
+
+		new Setting(containerEl)
+			.setName('Dict URL Scheme')
+			.setDesc('')
+			.addText(text => text
+				.setPlaceholder(PLUGIN_SETTINGS.dictURL)
+				.setValue(this.plugin.settings.dictURL)
+				.onChange(async (value) => {
+					this.plugin.settings.dictURL = value;
+					await this.plugin.saveSettings();
+				}));
+
+		new Setting(containerEl)
+			.setName('morphemeAnalysisAPI')
+			.setDesc("if you not know this, please don't change it")
+			.addText(text => text
+				.setPlaceholder(PLUGIN_SETTINGS.morphemeAnalysisAPI)
+				.setValue(this.plugin.settings.morphemeAnalysisAPI)
+				.onChange(async (value) => {
+					this.plugin.settings.morphemeAnalysisAPI = value;
+					await this.plugin.saveSettings();
+				}));
+	}
+}
+
+/**
+ * 写入剪贴板
+ * @param word 
+ */
+async function write2ClipBoard(word: string) {
+	try {
+		const searchWord = word;
+		await navigator.clipboard.writeText(searchWord);
+		console.log(`write 「${searchWord}」 to clipboard successfully`);
+		new Notice(`Search「${searchWord}」in MonoKakido by clipboard`);
+	} catch (err) {
+		console.error('can not write to clipboard :', err);
+	}
+}
+
+/**
+ * 通过调用 URL Scheme 打开辞书
+ * @param word 
+ */
+function openDictUrl(word: string) {
+	let dictUrl = "";
+	if (PLUGIN_SETTINGS.dictURL.includes("<text_to_search>")) {
+		console.log(`defaultDictURL is ${PLUGIN_SETTINGS.dictURL}, includes <text_to_search>`);
+		dictUrl = PLUGIN_SETTINGS.dictURL.replace("<text_to_search>", word);
+	} else if (PLUGIN_SETTINGS.dictURL.includes("{w}")) {
+		// 通用的网址链接{w}
+		console.log(`defaultDictURL is ${PLUGIN_SETTINGS.dictURL}, includes {w}`);
+		dictUrl = PLUGIN_SETTINGS.dictURL.replace("{w}", word);
+	}
+	else {
+		// 直接在 URL Scheme 末尾拼接上单词
+		console.log(`defaultDictURL is ${PLUGIN_SETTINGS.dictURL}`);
+		dictUrl = PLUGIN_SETTINGS.dictURL + word;
+	}
+	window.location.href = dictUrl;
+}
+
