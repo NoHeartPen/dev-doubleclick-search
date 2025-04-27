e393c8813d57efb5ca434be029e5f30083386e0a
noheartpen <3104408599@qq.com>
2025-04-27 14:37:07 +0900

    refactor: 封装打开辞典有关的函数

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { App, Editor, MarkdownView, Platform, Notice, Plugin, PluginSettingTab, Setting, TFile, normalizePath } from 'obsidian';
+import { openDictUrl, write2ClipBoard } from './utils/open-dict-utils';
 
 /**
  * 调试模式开关
@@ -8,7 +9,7 @@ const isDebug = false;
 /**
  * 调试日志
  */
-function debugLog(...args: string[]) {
+export function debugLog(...args: string[]) {
 	if (isDebug) {
 		console.log(...args);
 	}
@@ -221,7 +222,7 @@ interface PluginSettingsInterface {
 	historyFilePath: string;
 }
 
-const PLUGIN_SETTINGS: PluginSettingsInterface = {
+export const PLUGIN_SETTINGS: PluginSettingsInterface = {
 	// 默认使用 物書堂
 	dictURL: 'mkdictionaries:///?text=<text_to_search>',
 	// 默认不使用剪贴板查询模式
@@ -378,44 +379,3 @@ class SettingTab extends PluginSettingTab {
 				}));
 	}
 }
-
-/**
- * 将要查的单词写入剪贴板
- * @param word 需要要写入剪贴板的单词
- */
-async function write2ClipBoard(word: string) {
-	try {
-		await navigator.clipboard.writeText(word);
-		debugLog(`write 「${word}」 to clipboard successfully`);
-	} catch (err) {
-		console.error('can not write to clipboard :', err);
-	}
-}
-
-/**
- * 通过调用 URL Scheme 打开辞书
- * @param word 用于拼接 URL Scheme 的单词
- */
-function openDictUrl(word: string) {
-	let dictUrl = "";
-	debugLog(`openDictUrl: ${PLUGIN_SETTINGS.dictURL}`);
-	if (PLUGIN_SETTINGS.dictURL.includes("<text_to_search>")) {
-		// Monokakido URL Scheme is end with "<text_to_search>".
-		debugLog(`defaultDictURL is ${PLUGIN_SETTINGS.dictURL}, includes <text_to_search>`);
-		dictUrl = PLUGIN_SETTINGS.dictURL.replace("<text_to_search>", word);
-	} else if (PLUGIN_SETTINGS.dictURL.includes("<文字列>")) {
-		debugLog(`defaultDictURL is ${PLUGIN_SETTINGS.dictURL}, includes <文字列>`);
-		dictUrl = PLUGIN_SETTINGS.dictURL.replace("<文字列>", word);
-	} else if (PLUGIN_SETTINGS.dictURL.includes("{w}")) {
-		// 通用的网址链接{w}
-		debugLog(`defaultDictURL is ${PLUGIN_SETTINGS.dictURL}, includes {w}`);
-		dictUrl = PLUGIN_SETTINGS.dictURL.replace("{w}", word);
-	}
-	else {
-		// 直接在 URL Scheme 末尾拼接上单词
-		debugLog(`defaultDictURL is ${PLUGIN_SETTINGS.dictURL}`);
-		dictUrl = PLUGIN_SETTINGS.dictURL + word;
-	}
-	window.open(dictUrl, '_blank');
-}
-
